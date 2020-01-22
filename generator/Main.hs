ffe9e60131e5782529fe03a242ff194dfc86938a
artur <artur11jorge@gmail.com>
2020-01-22 17:05:44 +0000

    forcing exceptions in db

--- generator/Main.hs
+++ generator/Main.hs
@@ -8,11 +8,26 @@ data Tree = Tree {
     node :: String,
     bellow :: [Tree]
 }
-    deriving Show
+    deriving (Show, Eq)
 
 root :: Tree 
 root = Tree "" []
 
+exception :: Tree 
+exception = Tree "" 
+                [ Tree "Universidade do Minho" 
+                    [ Tree "Mestrado Integrado em Engenharia Informática"
+                        [ Tree "Desenvolvimento de Aplicações Web" 
+                            []
+                        , Tree "Processamento e Representação de Informação"
+                            []
+                        ]
+                    , Tree "Mestrado de Matemática e Computação"
+                        [ Tree "Desenvolvimento de Aplicações Web" 
+                            []
+                        ]
+                    ]
+                ]
 
 random_sub :: Eq a => Int -> [a] -> IO [a]
 random_sub n l  = aux_random_sub n l []
@@ -60,34 +75,38 @@ tree2group (Tree s l) = let
                     Group (Path (n: pt')) i' n' su' r' w' pg')
                 lg)
 
-create_tree :: [String] -> Int -> [String] -> Int -> [String] -> Int -> IO Tree
-create_tree ul a cl b dl c = 
-    generate_one_layer a ul root
-    >>= generate_one_layer b cl
-    >>= generate_one_layer c dl
+create_tree :: [([String], Int)] -> IO Tree
+create_tree l = create_tree_aux (reverse l)
+    where
+        create_tree_aux [] = return root
+        create_tree_aux ((l,n):t) = create_tree_aux t >>= generate_one_layer n l 
+
+union_tree :: Tree -> Tree -> Tree
+union_tree (Tree s1 []) t2@(Tree s2 l2) = t2
+union_tree (Tree s1 (h:t)) t2@(Tree s2 l2) 
+    | s1 == s2  = union_tree (Tree s1 t) (Tree s2 nl2)
+    | otherwise = t2
+    where 
+        (Just n) = (node h) `elemIndex` (map node l2)
+        nl2 = maybe 
+                (h:l2) 
+                (\n->(union_tree h (l2!!n)) : delete (l2!!n) l2) 
+                $ (node h) `elemIndex` (map node l2)
 
 
 main :: IO ()
 main = do
-    nomes_próprios <- readFile "nomes_próprios.txt"
-    apelidos <- readFile "apelidos.txt"
-    passwords <- readFile "passwords.txt"
-    universidades <- readFile "universidades.txt"
-    cursos <- readFile "cursos.txt"
-    disciplinas <- readFile "disciplinas.txt"
-    pages <- readFile "pages.txt"
-
-    let nomes_próprios_list = lines nomes_próprios
-    let apelidos_list = lines apelidos
-    let passwords_list = lines passwords
-    let universidades_list = lines universidades
-    let cursos_list = lines cursos
-    let disciplinas_list = lines disciplinas
-    let pages_list = lines pages
+    nomes_próprios  <- return . lines =<< readFile "nomes_próprios.txt"
+    apelidos        <- return . lines =<< readFile "apelidos.txt"
+    passwords       <- return . lines =<< readFile "passwords.txt"
+    universidades   <- return . lines =<< readFile "universidades.txt"
+    cursos          <- return . lines =<< readFile "cursos.txt"
+    disciplinas     <- return . lines =<< readFile "disciplinas.txt"
+    pages           <- return . lines =<< readFile "pages.txt"
 
     tucd <- create_tree 
-                universidades_list 10
-                cursos_list 10
-                disciplinas_list 10
+                [(universidades, 10)
+                ,(cursos, 10)
+                ,(disciplinas, 10)]
 
-    writeFile "groups.json" $ show $ tree2group $ tucd
+    writeFile "groups.json" $ show $ tree2group $ union_tree exception tucd
