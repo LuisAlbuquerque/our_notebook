f2c06d37ed0d0947422354555107e1d579c7c47a
LuisAlbuquerque <lmiguelaa@gmail.com>
2020-01-23 19:59:01 +0000

    init right bar

--- interface/public/javascripts/ournote.js
+++ interface/public/javascripts/ournote.js
@@ -153,4 +153,10 @@ const swap_elements = (path) => {
     }
 }
 
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
