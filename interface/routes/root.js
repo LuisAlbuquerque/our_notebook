b03592023d77d75b04e25a83701e29c5d5686b6c
LuisAlbuquerque <lmiguelaa@gmail.com>
2020-01-25 11:26:25 +0000

    read and write permissions (Interface)

--- interface/routes/root.js
+++ interface/routes/root.js
@@ -23,7 +23,7 @@ var api_get = path => res => result => {
         })
 }
 
-router.get('/*', verifyAuthentication, function(req, res, next) {
+router.get('/*', verifyAuthentication_read, function(req, res, next) {
     let path = req.params['0'].replace(/\/+$/, '');
     let path_list = path.split("/");
 
@@ -35,7 +35,7 @@ router.get('/*', verifyAuthentication, function(req, res, next) {
         });
 });
 
-router.post('/*', verifyAuthentication, function(req, res, next) {
+router.post('/*', verifyAuthentication_write, function(req, res, next) {
     let path = req.params['0'].replace(/\/+$/, '');
     let path_list = path.split("/");
 
@@ -57,7 +57,7 @@ router.post('/*', verifyAuthentication, function(req, res, next) {
     }
 });
 
-router.put('/*', verifyAuthentication, function(req, res, next) {
+router.put('/*', verifyAuthentication_write, function(req, res, next) {
     let path = req.params['0'].replace(/\/+$/, '');
     let path_list = path.split("/");
     console.dir(req.body)
@@ -70,7 +70,7 @@ router.put('/*', verifyAuthentication, function(req, res, next) {
         .catch(err => res.render('error', {error: err}));
 });
 
-router.delete('/*', verifyAuthentication, function(req, res, next) {
+router.delete('/*', verifyAuthentication_write, function(req, res, next) {
     let path = req.params['0'].replace(/\/+$/, '');
     let path_list = path.split("/");
     console.dir(req.body);
@@ -83,10 +83,36 @@ router.delete('/*', verifyAuthentication, function(req, res, next) {
         .catch(err => res.render('error', {error: err}));
 });
 
-function verifyAuthentication(req,res,next){
+function verifyAuthentication_read (req,res,next){
   if(req.isAuthenticated()){
   //req.isAuthenticated() will return true if user is logged in
-    next();
+    let path = req.params['0'].replace(/\/+$/, '');
+    axios.get(api_link + '/root/' + path)
+      .then(dados => {
+          if(dados.data.read_perm.includes(req.user.email) || path == ""){
+            next();
+          }
+          else{
+            res.redirect(interface_link + '/root');
+          }
+      })
+  } else{
+    res.redirect("/");}
+}
+
+function verifyAuthentication_write(req,res,next){
+  if(req.isAuthenticated()){
+  //req.isAuthenticated() will return true if user is logged in
+    let path = req.params['0'].replace(/\/+$/, '');
+    axios.get(api_link + '/root/' + path)
+      .then(dados => {
+          if(dados.data.write_perm.includes(req.user.email) || req.body.name != undefined){
+            next();
+          }
+          else{
+            res.redirect(interface_link + '/root/' + path);
+          }
+      })
   } else{
     res.redirect("/");}
 }
